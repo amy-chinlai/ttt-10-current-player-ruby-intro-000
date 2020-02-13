2fc5a896ab73b3a6c408c6b5ebd7d0bd84b8f241
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-02-13 19:31:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
-  turns=0
+  turns = 0
   board.each do |piece|
-  if(piece=="X" || piece=="O")
-  turns+=1
+  if (piece=="X" || piece=="O")
+  turns += 1
 end
 end
 end
 
 def current_player(board)
-  turn_count(board)%2==0?"X":"O"
+  turn_count(board)
 end
