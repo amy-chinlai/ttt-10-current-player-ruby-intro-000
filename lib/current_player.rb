85157c07c531374d4dc6ab3003b451bce3a4d11e
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-02-13 19:51:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,13 @@
 def turn_count(board)
-  turns = 0
-  board.each do |token|
-    if (token=="X" || token=="O")
-      turns += 1
-end
-end
+  turn=0
+  board.each do |item|
+    if(item=="O" || item=="X")
+      turn+=1
+    end
+  end
+  turn
 end
 
 def current_player(board)
-  turn_count(board) % 2 == 0? "X": "O"
+  turn_count(board)%2==0?"X":"O"
 end
