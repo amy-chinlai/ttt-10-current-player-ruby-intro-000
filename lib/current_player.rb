f650d2b4f967ecc97f56e9ec0aa6bb188d6f6a9c
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-02-13 19:25:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   turns = 0
   board.each do |piece|
-  if (piece == "X" || piece =="O")
+  if (piece == "X" || piece == "O")
   turns += 1
 end
 end
